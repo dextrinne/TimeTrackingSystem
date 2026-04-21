2039d3e05e0b4da858ca7f20bc49799bb0d4fb74
dextrinne <167472397+dextrinne@users.noreply.github.com>
2026-04-21 23:25:49 +0300

    Удаление старой структуры

--- frontend/__init__.py
+++
@@ -1,4 +0,0 @@
-from .styles import DayTypes, Colors, Fonts
-from .config import AppConfig
-
-__all__ = ['DayTypes', 'Colors', 'Fonts', 'AppConfig']
--- ui/__init__.py
+++
@@ -1,5 +0,0 @@
-from .main_window import MainWindow
-from .dialogs import EmployeeDialog
-from .widgets import CalendarGrid, DayButton
-
-__all__ = ['MainWindow', 'EmployeeDialog', 'CalendarGrid', 'DayButton']
