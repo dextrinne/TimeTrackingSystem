1a509667077d3c98dff0805f58d7aa2545de9dce
dextrinne <167472397+dextrinne@users.noreply.github.com>
2026-04-22 18:43:53 +0300

    cleaning

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .time_table_app import TimeTableApp
-from .data_manager import DataManager
-from .template_exporter import TemplateExporter
-
-__all__ = ['TimeTableApp', 'DataManager', 'TemplateExporter']
--- frontend/__init__.py
+++
@@ -1,4 +0,0 @@
-from .styles import DayTypes, AppStyle
-from .config import AppConfig, UIConfig
-
-__all__ = ['DayTypes', 'AppStyle', 'AppConfig', 'UIConfig']
